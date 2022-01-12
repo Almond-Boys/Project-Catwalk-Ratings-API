715cd644cfeed37f6bf7209ad555b6e2106d50fe
Justin Le <justinhung.le@gmail.com>
2022-01-12 10:09:59 -0800

    Added constraints to my Postgres Schema

--- database/schema-example.sql
+++ database/schema-example.sql
@@ -7,16 +7,16 @@ USE reviewsAPI;
 CREATE TABLE reviews (
   id BIGSERIAL NOT NULL UNIQUE AUTO_INCREMENT,
   product_id INTEGER,
-  rating INTEGER,
-  date BIGINT,
-  summary VARCHAR,
-  body VARCHAR,
+  rating INTEGER CONSTRAINT valid_rating CHECK (rating > 0 AND rating < 6),
+  date date,
+  summary VARCHAR(60),
+  body VARCHAR(1000),
   recommend BOOLEAN,
   reported BOOLEAN,
-  reviewer_name VARCHAR,
-  reviewer_email VARCHAR,
-  response VARCHAR,
-  helpfulness INTEGER
+  reviewer_name VARCHAR(50),
+  reviewer_email VARCHAR(50),
+  response VARCHAR(1000),
+  helpfulness BIGSERIAL NULL
 );
 
 CREATE TABLE photos (
@@ -28,16 +28,16 @@ CREATE TABLE photos (
 CREATE TABLE characteristics (
   id BIGSERIAL NOT NULL UNIQUE,
   product_id INTEGER,
-  name VARCHAR
+  characteristics_name VARCHAR(100)
 );
 
 CREATE TABLE review_characteristics (
   id BIGSERIAL NOT NULL UNIQUE,
   review_id INTEGER,
   characteristic_id INTEGER,
-  value INTEGER
+  characteristics_rating INTEGER CONSTRAINT valid_characteristics_rating CHECK (characteristics_rating > 0 AND rating < 6)
 );
 
 ALTER TABLE photos ADD FOREIGN KEY (review_id) REFERENCES reviews(id);
-ALTER TABLE review_characteristics ADD FOREIGN KEY (review_id) REFERENCES reviews(id;
+ALTER TABLE review_characteristics ADD FOREIGN KEY (review_id) REFERENCES reviews(id);
 ALTER TABLE review_characteristics ADD FOREIGN KEY (characteristic_id) REFERENCES characteristics(id);
