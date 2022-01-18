014046246cceabf016f402c86d5ce4737f22e04b
Justin Le <justinhung.le@gmail.com>
2022-01-18 15:27:11 -0800

    Fixed Test

--- k6testing.js
+++ k6testing.js
@@ -1,14 +1,12 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-import { Counter } from 'k6/metrics';
 
-export const requests = new Counter('http_reqs');
 export const options = {
   vus: 100,
   duration: '15s',
 };
 
-const url = 'http://localhost:3030/reviews/';
+const url = 'http://localhost:3030/reviews/meta/';
 
 export default () => {
   const res = http.get(url);
